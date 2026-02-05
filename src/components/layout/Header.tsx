b9b0dca7f433af1716d6e7b222f1d75e56772df5
Jadest13 <jadest13x03@gmail.com>
2026-02-05 17:00:29 +0900

    feat(layout): hamburger to x svg

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,22 +1,27 @@
+"use client";
+
 import Link from "next/link";
 import LogoSVG from "@/assets/dokai.svg";
+import SearchSVG from "@/assets/icons/search.svg";
+import { useState } from "react";
+import HamburgerXSVG from "@/assets/icons/hamburger-x.svg";
 
 const Header = () => {
+  const [openDrawer, setOpenDrawer] = useState(false);
+
   return (
-    <header>
-      <LogoSVG />
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/about">About</Link>
-          </li>
-          <li>
-            <Link href="/works">Works</Link>
-          </li>
-        </ul>
+    <header className="flex items-center justify-between absolute top-0 z-50 w-full p-4 bg-white">
+      <Link href="/">
+        <LogoSVG />
+      </Link>
+      <nav className="flex items-center gap-4">
+        <Link href="/about">About</Link>
+        <Link href="/works">Works</Link>
+        <SearchSVG />
+        <HamburgerXSVG
+          data-open={openDrawer}
+          onClick={() => setOpenDrawer(!openDrawer)}
+        />
       </nav>
     </header>
   );
