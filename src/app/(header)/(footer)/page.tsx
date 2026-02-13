feb3414e7fa270cd9459d280f50d100e2cf61884
Jadest13 <jadest13x03@gmail.com>
2026-02-13 20:37:36 +0900

    feat(api): 이미지 업로드 구현

--- src/app/(header)/(footer)/page.tsx
+++ src/app/(header)/(footer)/page.tsx
@@ -1,13 +1,32 @@
+"use client";
+
 import * as Styles from "./style.css";
 import MainWorks from "./MainWorks";
+import { uploadImage } from "@utils/uploadImage";
 
 export default function Home() {
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+    const { key, url, deduped } = await uploadImage(file);
+    console.log(key, url, deduped);
+  };
+
   return (
     <div className={`${Styles.Container} page-wrapper layout-wrapper`}>
       <p className={Styles.Title}>
         DOKAI was founded by professionals from the commercial film industry as
         an AI-driven creative group.
       </p>
+      <input
+        type="file"
+        style={{
+          position: "absolute",
+          zIndex: "1000",
+        }}
+        onChange={handleFileChange}
+      />
+
       <MainWorks />
     </div>
   );
