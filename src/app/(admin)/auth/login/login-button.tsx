74aeb8d0281bf5028b1adfbfe72f6a03b8ed8bde
Jadest13 <jadest13x03@gmail.com>
2026-03-05 04:06:27 +0900

    fix(chore): login callback next

--- src/app/(admin)/auth/login/login-button.tsx
+++ src/app/(admin)/auth/login/login-button.tsx
@@ -10,7 +10,7 @@ export default function LoginButton() {
 
   const handleLogin = async () => {
     const supabase = createSupabaseBrowserClient();
-    const next = location.pathname + location.search;
+    const next = location.href;
 
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: "google",
