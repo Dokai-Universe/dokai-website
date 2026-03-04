84a57527809fe7b329d8b45b4d6c660cb0522f0e
Jadest13 <jadest13x03@gmail.com>
2026-03-05 01:41:11 +0900

    fix(remove): has-profile api remove

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -84,8 +84,8 @@ export default function RootLayout({
         <Providers>
           <Suspense fallback={null}>
             <ModalStackRoot />
+            {children}
           </Suspense>
-          {children}
         </Providers>
       </body>
     </html>
